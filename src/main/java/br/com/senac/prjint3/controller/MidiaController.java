dad24094b808742e849243d5578fcb4f7d1eb325
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:01:39 -0300

    Update MidiaController.java

--- src/main/java/br/com/senac/prjint3/controller/MidiaController.java
+++ src/main/java/br/com/senac/prjint3/controller/MidiaController.java
@@ -9,12 +9,14 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/midias")
 @Tag(name = "Mídias", description = "CRUD de mídias com apagado lógico")
+@CrossOrigin("*")    
 public class MidiaController {
     private final MidiaService service;
 

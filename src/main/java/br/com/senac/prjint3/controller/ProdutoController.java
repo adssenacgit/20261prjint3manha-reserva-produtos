95fa1b9cd86209d5017124d3437ee9800a530ec0
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:01:47 -0300

    Update ProdutoController.java

--- src/main/java/br/com/senac/prjint3/controller/ProdutoController.java
+++ src/main/java/br/com/senac/prjint3/controller/ProdutoController.java
@@ -9,12 +9,14 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/produtos")
 @Tag(name = "Produtos", description = "CRUD de produtos com apagado lógico")
+@CrossOrigin("*")    
 public class ProdutoController {
     private final ProdutoService service;
 
